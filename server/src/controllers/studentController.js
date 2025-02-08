823131aa04e4a017dbcf47d721db6440bd52bd10
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2025-02-08 21:15:29 +0800

    search and delete students done server

--- server/src/controllers/studentController.js
+++ server/src/controllers/studentController.js
@@ -12,6 +12,58 @@ const getStudents = async (req, res) => {
           required: true,
         },
       ],
+      order: [["createdAt", "DESC"]],
+    });
+    return res.status(200).json(students);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteStudent = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await userModel.destroy({ where: { id } });
+    return res
+      .status(200)
+      .json({ message: "Student deleted successfully", status: "success" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const filterStudents = async (req, res) => {
+  const { name, course, yearLevel } = req.query;
+
+  // Conditions for userModel (firstName, lastName)
+  const userWhereCondition = {};
+  if (name) {
+    userWhereCondition[Op.or] = [
+      { firstName: { [Op.like]: `${name}%` } },
+      { lastName: { [Op.like]: `${name}%` } },
+    ];
+  }
+
+  // Conditions for studentModel (course, yearLevel)
+  const studentWhereCondition = {};
+  if (course) {
+    studentWhereCondition.course = course;
+  }
+  if (yearLevel) {
+    studentWhereCondition.yearLevel = yearLevel;
+  }
+
+  try {
+    const students = await userModel.findAll({
+      where: userWhereCondition,
+      include: [
+        {
+          model: studentModel,
+          required: true,
+          where: studentWhereCondition,
+        },
+      ],
+      order: [["createdAt", "DESC"]],
     });
     return res.status(200).json(students);
   } catch (error) {
@@ -21,4 +73,6 @@ const getStudents = async (req, res) => {
 
 module.exports = {
   getStudents,
+  deleteStudent,
+  filterStudents,
 };
