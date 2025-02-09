9b4fc94e0864bc43b643996b3d19acd6620cc2d1
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2025-02-09 23:43:26 +0800

    search instructor done

--- client/src/pages/Admin/Instructor/Instructor.jsx
+++ client/src/pages/Admin/Instructor/Instructor.jsx
@@ -1,26 +1,37 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllStudents, fetchStudents } from "../../../services/studentSlice";
 import Pagination from "../../../components/Pagination";
 import { IoSearch } from "react-icons/io5";
-import StudentsTable from "../../../components/table/StudentsTable";
+import UserTable from "../../../components/table/UserTable";
+import AddInstructor from "./AddInstructor";
+import {
+  fetchUsers,
+  getAllUsers,
+  searchInstructor,
+} from "../../../services/usersSlice";
+import rolesList from "../../../constants/rolesList";
 
 const Instructor = () => {
   const dispatch = useDispatch();
-  const allStudents = useSelector(getAllStudents);
+  const instructors = useSelector(getAllUsers);
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
 
   const dataPerPage = 10;
 
   useEffect(() => {
-    dispatch(fetchStudents());
-  }, [dispatch]);
+    if (searchTerm) {
+      dispatch(searchInstructor(searchTerm));
+    } else {
+      dispatch(fetchUsers(rolesList.instructor));
+    }
+  }, [dispatch, searchTerm]);
 
   // Paganation
   const indexOfLastDocument = currentPage * dataPerPage;
   const indexOfFirstDocument = indexOfLastDocument - dataPerPage;
-  const currentData = allStudents?.slice(
+  const currentData = instructors?.slice(
     indexOfFirstDocument,
     indexOfLastDocument
   );
@@ -31,14 +42,20 @@ const Instructor = () => {
     <div>
       <div className="flex  gap-5  flex-col">
         <div className="flex flex-col md:flex-row justify-between  w-full  gap-3">
-          <button className="text-nowrap w-fit p-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded-md">
-            New Instructor
+          <button
+            onClick={() => setShowModal(true)}
+            className="text-nowrap w-fit p-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded-md"
+          >
+            Add Instructor
           </button>
+          {showModal && (
+            <AddInstructor setShowModal={setShowModal} showModal={showModal} />
+          )}
           <div className="flex justify-end  w-full  gap-3">
             <div className=" flex w-full lg:w-1/2  items-center relative">
               <input
                 type="text"
-                placeholder="Search student..."
+                placeholder="Search instructor..."
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="border border-blue-500 focus:border-blue rounded-xl w-full bg-gray-100 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -50,11 +67,11 @@ const Instructor = () => {
       </div>
 
       <div className="mt-8">
-        <StudentsTable allStudents={currentData} />
+        <UserTable allStudents={currentData} />
         <div className="flex justify-end mt-5">
           <Pagination
             dataPerPage={dataPerPage}
-            totalData={allStudents?.length}
+            totalData={instructors?.length}
             paginate={paginate}
             currentPage={currentPage}
           />
