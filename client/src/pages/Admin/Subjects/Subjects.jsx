9be7d808631b20d25b215924888356ad3287b40b
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2025-02-09 23:49:09 +0800

    search subject done

--- client/src/pages/Admin/Subjects/Subjects.jsx
+++ client/src/pages/Admin/Subjects/Subjects.jsx
@@ -1,21 +1,31 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllSubjects, fetchSubjects } from "../../../services/subjectSlice";
+import {
+  getAllSubjects,
+  fetchSubjects,
+  searchSubject,
+} from "../../../services/subjectSlice";
 import Pagination from "../../../components/Pagination";
 import { IoSearch } from "react-icons/io5";
 import SubjectTable from "../../../components/table/SubjectTable";
+import AddSubject from "./AddSubject";
 
-const Instructor = () => {
+const Subject = () => {
   const dispatch = useDispatch();
   const allStudents = useSelector(getAllSubjects);
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
 
   const dataPerPage = 10;
 
   useEffect(() => {
-    dispatch(fetchSubjects());
-  }, [dispatch]);
+    if (searchTerm) {
+      dispatch(searchSubject(searchTerm));
+    } else {
+      dispatch(fetchSubjects());
+    }
+  }, [dispatch, searchTerm]);
 
   // Paganation
   const indexOfLastDocument = currentPage * dataPerPage;
@@ -31,14 +41,21 @@ const Instructor = () => {
     <div>
       <div className="flex  gap-5  flex-col">
         <div className="flex flex-col md:flex-row justify-between  w-full  gap-3">
-          <button className="text-nowrap w-fit p-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded-md">
-            New Subject
+          <button
+            onClick={() => setShowModal(true)}
+            className="text-nowrap w-fit p-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded-md"
+          >
+            Add Subject
           </button>
+          {showModal && (
+            <AddSubject showModal={showModal} setShowModal={setShowModal} />
+          )}
+
           <div className="flex justify-end  w-full  gap-3">
             <div className=" flex w-full lg:w-1/2  items-center relative">
               <input
                 type="text"
-                placeholder="Search student..."
+                placeholder="Search subject..."
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="border border-blue-500 focus:border-blue rounded-xl w-full bg-gray-100 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -64,4 +81,4 @@ const Instructor = () => {
   );
 };
 
-export default Instructor;
+export default Subject;
