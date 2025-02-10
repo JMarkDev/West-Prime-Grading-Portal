6f93158d305eda90f577af78fae0ed0522f6ade4
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2025-02-10 20:39:55 +0800

    student slice done

--- client/src/services/studentSlice.js
+++ client/src/services/studentSlice.js
@@ -9,10 +9,42 @@ export const fetchStudents = createAsyncThunk(
   }
 );
 
+export const deleteStudent = createAsyncThunk(
+  "students/deleteStudent",
+  async ({ id, toast }) => {
+    const response = await axios.delete(`/students/delete/${id}`);
+
+    if (response.data.status === "success") {
+      toast.success(response.data.message);
+      return id;
+    }
+  }
+);
+
 export const fetchStudentById = createAsyncThunk(
   "students/fetchStudentById",
   async (id) => {
     const response = await axios.get(`/students/${id}`);
+    return response.data;
+  }
+);
+
+export const filterStudents = createAsyncThunk(
+  "students/filterStudents",
+  async ({ name, course, yearLevel }) => {
+    const queryParams = new URLSearchParams();
+
+    if (name) {
+      queryParams.append("name", name);
+    }
+    if (course) {
+      queryParams.append("course", course);
+    }
+    if (yearLevel) {
+      queryParams.append("yearLevel", yearLevel);
+    }
+
+    const response = await axios.get(`/students/filter?${queryParams}`);
     return response.data;
   }
 );
@@ -51,6 +83,30 @@ const studentSlice = createSlice({
       })
       .addCase(fetchStudentById.rejected, (state, action) => {
         state.status.studentById = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(deleteStudent.pending, (state) => {
+        state.status.students = "loading";
+      })
+      .addCase(deleteStudent.fulfilled, (state, action) => {
+        state.status.students = "succeeded";
+        state.allStudents = state.allStudents.filter(
+          (student) => student.id !== action.payload
+        );
+      })
+      .addCase(deleteStudent.rejected, (state, action) => {
+        state.status.students = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(filterStudents.pending, (state) => {
+        state.status.students = "loading";
+      })
+      .addCase(filterStudents.fulfilled, (state, action) => {
+        state.status.students = "succeeded";
+        state.allStudents = action.payload;
+      })
+      .addCase(filterStudents.rejected, (state, action) => {
+        state.status.students = "failed";
         state.error = action.error.message;
       });
   },
